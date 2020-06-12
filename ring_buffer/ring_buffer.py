5896602902a7960c0d8cc73abd4f3c2f2473b70b
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-06-12 10:41:24 +0100

    solution for ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,18 +1,43 @@
-from singly_linked_list import LinkedList
+from doubly_linked_list import DoublyLinkedList
 
 
 class RingBuffer:
     def __init__(self, capacity):
-        self.size = 0
+        # self.size = 0
+        self.current = None
         self.capacity = capacity
-        self.storage = []
+        self.storage = DoublyLinkedList()
 
     def append(self, item):
-        if self.size == self.capacity:
-            self.cur = 0
-            self.storage[self.cur] = x
-            self.cur = (self.cur + 1) % self.capacity
-        self.storage.append(item)
+        if self.storage.length == 0:
+            self.storage.add_to_head(item)
+            self.current = self.storage.head
+
+        elif self.storage.length < self.capacity:
+            self.storage.add_to_tail(item)
+            self.current = self.current.next
+
+        else:
+            if not self.current.next:
+                self.storage.remove_from_head()
+                self.storage.add_to_head(item)
+                self.current = self.storage.head
+
+            else:
+                self.current.next.delete()
+                self.current.insert_after(item)
+                self.current = self.current.next
 
     def get(self):
-        return self.storage[self.cur:]+self.storage[:self.cur]
+        list_buffer_contents = []
+
+        def add_to_list(node):
+            if not node:
+                return
+            else:
+                list_buffer_contents.append(node.value)
+                add_to_list(node.next)
+
+        add_to_list(self.storage.head)
+
+        return list_buffer_contents
